6b36120bca9aff032b28ba6018be857734557b07
Nikita <fomin.nv@phystech.edu>
2024-11-16 16:00:29 +0300

    method get_data

--- CProduct.php
+++ CProduct.php
@@ -1,7 +1,20 @@
 <?php
 
 class CProduct {
-    public function get_data(int $number) {
-        
+    public static function get_data(int $number) {
+        $conn = new mysqli('localhost', 'root', 'root');
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $query = "SELECT * FROM test_task_db.products ORDER BY date_create DESC LIMIT " . $number;
+        if ($data = $conn->query($query)) {
+            return $data;
+        } else {
+        echo "Ошибка: " . $conn->error;
+    }
+
+    $conn->close();
+    return false;
+
     }
 };

1e34030a10412237718778c75b755948f5a4731c
Nikita <fomin.nv@phystech.edu>
2024-11-16 17:44:50 +0300

    table

--- CProduct.php
+++ CProduct.php
@@ -1,14 +1,22 @@
 <?php
 
 class CProduct {
-    public static function get_data(int $number) {
+    public static function get_data(int $number, string $order='DESC') {
         $conn = new mysqli('localhost', 'root', 'root');
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        $query = "SELECT * FROM test_task_db.products ORDER BY date_create DESC LIMIT " . $number;
-        if ($data = $conn->query($query)) {
-            return $data;
+        $query = "SELECT * FROM test_task_db.products ORDER BY date_create " . $order . " LIMIT " . $number;
+        if ($data = $conn->query($query)->fetch_all(MYSQLI_ASSOC)) {
+            $columns = array_keys($data[0]);
+            unset($columns[array_search('id', $columns)]);
+            unset($columns[array_search('is_hidden', $columns)]);
+            for($i = 0; $i < count($data); ++$i) {
+                $row = &$data[$i];
+                unset($row['id']);
+                unset($row['is_hidden']);
+            }
+            return [$columns, $data];
         } else {
         echo "Ошибка: " . $conn->error;
     }
--- index.php
+++ index.php
@@ -6,9 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <table>
+    <table style='border: 3px solid; border-collapse: collapse; width: 100%;'>
         <?php
-            
+            require_once('./CProduct.php');
+            [$columns, $data] = CProduct::get_data(20);
+            echo "<thead>";
+            foreach($columns as $column) {
+                echo "<th style='border: 3px solid; border-collapse: collapse;'>" . $column . "</th>";
+            }
+            echo "</thead>";
+            foreach($data as $row) {
+                echo "<tr style='border: 3px solid; border-collapse: collapse;'>";
+                foreach($row as $cell) {
+                    echo "<td style='border: 3px solid; border-collapse: collapse;'>" . $cell . "</td>";
+                }
+                echo "</tr>";
+            }
         ?>
     </table>
 </body>
